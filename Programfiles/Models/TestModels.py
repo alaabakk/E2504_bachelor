514db75e4d5930d1d9a5e57fb541462e69502134
olefhau <olefhau@stud.ntnu.no>
2025-03-24 15:31:40 +0100

    Test

--- Programfiles/Models/TestModels.py
+++ Programfiles/Models/TestModels.py
@@ -1,9 +1,8 @@
 from ultralytics import YOLO
  
  
-#print("Sarting to predict")
-model = YOLO("yolo11s.pt", task="detect")
- 
+#print("Starting to predict")
+model = YOLO("yolov5n.pt", task="detect")
  
  
 results = model.predict("people-metro-station.mp4", save=True, stream=True, conf=0.5, iou=0.4)
