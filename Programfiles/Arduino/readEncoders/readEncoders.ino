c0068c3f42d6a9d8bc175424b14d5f723d48d0fb
kjethom <kjethom@stud.ntnu.no>
2025-04-03 11:38:01 +0200

    Added servo control to the readEncoders example

--- Programfiles/Arduino/readEncoders/readEncoders.ino
+++ Programfiles/Arduino/readEncoders/readEncoders.ino
@@ -1,27 +1,43 @@
+#include <ESP32Servo.h>
+
 #define ENC_ROT_A 32
 #define ENC_ROT_B 33
-
 #define ENC_TILT_A 34
 #define ENC_TILT_B 35
-
 #define PPR 1024
+
+#define SERVO1_PIN 18
+#define SERVO2_PIN 19
+#define SERVO_PWM_FREQ 50
+#define PWM_RESOLUTION 16
+
+Servo myservo1;
+Servo myservo2;
 
 volatile long encoderCountRot = 0;
 volatile long encoderCountTilt = 0;
 
 void IRAM_ATTR encoderFuncRot() {
   if (digitalRead(ENC_ROT_B) == LOW) {
-    encoderCountRot++;
+    if (encoderCountRot < 1024) {
+      encoderCountRot++;
+    }
   } else {
-    encoderCountRot--;
+    if (encoderCountRot > 0) { 
+      encoderCountRot--;
+    }
   }
 }
 
 void IRAM_ATTR encoderFuncTilt() {
   if (digitalRead(ENC_TILT_B) == LOW) {
+    if (encoderCountTilt > 0) {
     encoderCountTilt--;
+    }
   } else {
+    if (encoderCountTilt < 1024) {
     encoderCountTilt++;
+    }
   }
 }
 
@@ -36,6 +52,11 @@ void setup() {
   pinMode(ENC_TILT_A, INPUT);
   pinMode(ENC_TILT_B, INPUT);
   attachInterrupt(digitalPinToInterrupt(ENC_TILT_A), encoderFuncTilt, RISING);
+
+  myservo1.attach(SERVO1_PIN);
+  myservo2.attach(SERVO2_PIN);
+  myservo1.attach(SERVO1_PIN, 500, 2400);
+  myservo2.attach(SERVO2_PIN, 500, 2400);
 }
 
 void loop() {
@@ -52,8 +73,12 @@ static unsigned long lastPrint = 0;
     count2 = encoderCountTilt;
     interrupts();
 
+    // Convert encoder counts to angle (clamp between 0–180)
     float angleRot = (360.0 * count1) / PPR;
     float angleTilt = (360.0 * count2) / PPR;
+
+    myservo1.write(angleRot);
+    myservo2.write(angleTilt);
 
     // Print both angles
     Serial.print("Encoder Rot: ");
