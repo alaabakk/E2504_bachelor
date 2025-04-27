3ce6137013a28c9b833aac3157296cd84aec07fd
kjethom <kjethom@stud.ntnu.no>
2025-04-27 14:49:52 +0000

    added medium model

--- Models/ConvertToTensorRT.py
+++ Models/ConvertToTensorRT.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
 # Load model
-model = YOLO("yolov8s.pt")
+model = YOLO("yolov8m.pt")
 
 model.export(format='engine', device=0, half=True)  # export to TensorRT engine
