52135ce4a51d809b674848fb14afe0f7f2bd9ea7
kjethom <kjethom@stud.ntnu.no>
2025-04-27 12:51:57 +0200

    Changed ConvertToTensorRT to FP16

--- Models/ConvertToTensorRT.py
+++ Models/ConvertToTensorRT.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 # Load model
 model = YOLO("yolov8x.pt")
 
-model.export(format='engine', device=0)  # export to TensorRT engine
+model.export(format='engine', device=0, half=True)  # export to TensorRT engine
