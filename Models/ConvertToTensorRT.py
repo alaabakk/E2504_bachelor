dec74c1f198040c0e39fcba52ba6fcd69c9bebd0
kjethom <kjethom@stud.ntnu.no>
2025-04-27 14:05:17 +0200

    ConvertToTensorRT nano FP16

--- Models/ConvertToTensorRT.py
+++ Models/ConvertToTensorRT.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
 # Load model
-model = YOLO("yolov8x.pt")
+model = YOLO("yolov8n.pt")
 
 model.export(format='engine', device=0, half=True)  # export to TensorRT engine
