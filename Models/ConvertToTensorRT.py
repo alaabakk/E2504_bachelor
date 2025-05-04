bea5f1170c2f62622635d28af5492557e52398a0
Arthur Aabakken <alaabakk@stud.ntnu.no>
2025-05-04 16:33:41 +0200

    clean

--- Models/ConvertToTensorRT.py
+++ Models/ConvertToTensorRT.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
 # Load model
-model = YOLO("yolov8l.pt")
+model = YOLO("customYolov8n.pt")
 
 model.export(format='engine', device=0, half=True)  # export to TensorRT engine
