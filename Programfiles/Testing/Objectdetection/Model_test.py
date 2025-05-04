3d2f86d863dd8975eda07ed05d0fd0df3d874527
pettedig <pettedig@stud.ntnu.no>
2025-05-04 12:39:19 +0200

    Testing models on Jetson

--- Programfiles/Testing/Objectdetection/Model_test.py
+++ Programfiles/Testing/Objectdetection/Model_test.py
@@ -9,7 +9,7 @@ model = YOLO(model_path)  # or 'path/to/your/model.pt'
 # Add path to dataset.yaml
 data_path = os.path.join(script_dir, "/Testset/Distance.v1i.yolov8/data.yaml")
 # Evaluate the model on your dataset
-metrics = model.val(data_path, imgsz=640, batch=16, split="test", classes = [0],)
+metrics = model.val(data=data_path, imgsz=640, batch=16, split="test", classes = [0],)
 
 # Print results
 print(metrics)
