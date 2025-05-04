43c01d799d999d03ca427f184590b7781d143f29
pettedig <pettedig@stud.ntnu.no>
2025-05-04 12:20:56 +0200

    Changed test script

--- Programfiles/Testing/Objectdetection/Model_test.py
+++ Programfiles/Testing/Objectdetection/Model_test.py
@@ -5,16 +5,13 @@ model_path = os.path.join(script_dir, "../../../Models/yolov8n.engine")
 # Load your model (change path if needed)
 model = YOLO(model_path)  # or 'path/to/your/model.pt'
 
-data_path = os.path.join(script_dir, "")
+# Add path to dataset.yaml
+data_path = os.path.join(script_dir, "/Testset/Distance.v1i.yolov8/data.yaml")
 # Evaluate the model on your dataset
-metrics = model.val(data=r'C:\Users\Pette\OneDrive\Dokumenter\Skole\Bachelor\Code\Distance.v1i.yolov8\data.yaml', imgsz=640, batch=16, split="test", half=True, classes = [0],)
+metrics = model.val(data_path, imgsz=640, batch=16, split="test", classes = [0],)
 
 # Print results
 print(metrics)
-
-map_class0 = metrics.box.maps[0]  # for mAP@0.5
-print(f"mAP@0.5 for class 0 (person): {map_class0:.4f}")
-print()
 
 f1_class0 = metrics.box.f1[0]
 print(f"F1-score for class 0 (person): {f1_class0:.4f}")
