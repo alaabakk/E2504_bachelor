e1a553fc802ea3253a687f5beafd722cd18ed107
pettedig <pettedig@stud.ntnu.no>
2025-05-04 12:42:21 +0200

    testing models

--- Programfiles/Testing/Objectdetection/Model_test.py
+++ Programfiles/Testing/Objectdetection/Model_test.py
@@ -2,12 +2,12 @@ from ultralytics import YOLO
 import os
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(script_dir, "../../../Models/yolov8n.engine")
+model_path = os.path.join(script_dir, '../../../Models/yolov8n.engine')
 # Load your model (change path if needed)
 model = YOLO(model_path)  # or 'path/to/your/model.pt'
 
 # Add path to dataset.yaml
-data_path = os.path.join(script_dir, "/Testset/Distance.v1i.yolov8/data.yaml")
+data_path = os.path.join(script_dir, r'/Testset/Distance.v1i.yolov8/data.yaml')
 # Evaluate the model on your dataset
 metrics = model.val(data=data_path, imgsz=640, batch=16, split="test", classes = [0],)
 
