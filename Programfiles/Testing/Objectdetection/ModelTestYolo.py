4a88e09e2098abd531d09ac6b0688817eb4d610e
pettedig <pettedig@stud.ntnu.no>
2025-05-10 08:17:50 +0200

    Changed path

--- Programfiles/Testing/Objectdetection/ModelTestYolo.py
+++ Programfiles/Testing/Objectdetection/ModelTestYolo.py
@@ -2,14 +2,14 @@ from ultralytics import YOLO
 import os
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(script_dir, 'C:\Users\Pette\OneDrive\Dokumenter\Skole\Bachelor\Code\E2504_bachelor\Programfiles\Testing\Objectdetection\runs\detect\customYolov8n.pt')
+model_path = os.path.join(script_dir, '../../../Models/customYolo8n.pt')
 # Load your model (change path if needed)
 model = YOLO(model_path)  # or 'path/to/your/model.pt'
 
 # Add path to dataset.yaml
-data_path = os.path.join(script_dir, 'Testset/TrainingDataset/data.yaml')
+data_path = os.path.join(script_dir, 'Testset/Training/data.yaml')
 # Evaluate the model on your dataset
-metrics = model.val(data=data_path, imgsz=640, batch=16, split="test",)
+metrics = model.val(data=data_path, imgsz=640, batch=16, split="test", half=True, classes = [0],)
 
 #print metrics
 precision_class0 = metrics.box.p[0]
